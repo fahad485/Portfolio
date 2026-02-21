8baa5a5d66e0e4351beba39580986e46c73dd012
Md. Fahad Badsha Shamim <86362674+fahad485@users.noreply.github.com>
2026-02-21 13:10:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("darkToggle").onclick = () => {
+  document.body.classList.toggle("dark");
+};
+
+const faders = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 50) {
+      el.classList.add("show");
+    }
+  });
+});
